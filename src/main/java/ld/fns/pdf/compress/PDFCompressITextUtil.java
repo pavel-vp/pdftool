dff6c5aef7c08c9bc34199d0d533c187f49a41cc
pavel-vp <pinegin.pavel@gmail.com>
2020-02-08 14:20:34 +0400

    Small changes

--- src/main/java/ld/fns/pdf/compress/PDFCompressITextUtil.java
+++ src/main/java/ld/fns/pdf/compress/PDFCompressITextUtil.java
@@ -69,7 +69,7 @@ public class PDFCompressITextUtil {
 
                 int width = (int)(bi.getWidth() * downscaleFactor);
                 int height = (int)(bi.getHeight() * downscaleFactor);
-                BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_USHORT_GRAY);
                 AffineTransform at = AffineTransform.getScaleInstance(downscaleFactor, downscaleFactor);
                 Graphics2D g = img.createGraphics();
                 g.drawRenderedImage(bi, at);
@@ -84,7 +84,7 @@ public class PDFCompressITextUtil {
                 stream.put(PdfName.WIDTH, new PdfNumber(width));
                 stream.put(PdfName.HEIGHT, new PdfNumber(height));
                 stream.put(PdfName.BITSPERCOMPONENT, new PdfNumber(8));
-                stream.put(PdfName.COLORSPACE, PdfName.DEVICERGB);
+                stream.put(PdfName.COLORSPACE, PdfName.DEVICEGRAY);
             }
         }
         // Save altered PDF
