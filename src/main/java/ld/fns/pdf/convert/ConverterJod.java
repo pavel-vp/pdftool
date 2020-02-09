01ef7d70c3f3abd49b857a8b73fae73878f60c0b
pavel-vp <pinegin.pavel@gmail.com>
2020-02-09 13:43:33 +0400

    Фиксированный путь до опенофиса и логирование

--- src/main/java/ld/fns/pdf/convert/ConverterJod.java
+++ src/main/java/ld/fns/pdf/convert/ConverterJod.java
@@ -5,11 +5,10 @@ import org.jodconverter.document.DefaultDocumentFormatRegistry;
 import org.jodconverter.document.DocumentFormat;
 import org.jodconverter.office.LocalOfficeManager;
 import org.jodconverter.office.OfficeException;
+import org.jodconverter.office.OfficeManager;
 import org.jodconverter.office.OfficeUtils;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class ConverterJod {
 
@@ -17,7 +16,13 @@ public class ConverterJod {
         DocumentFormat documentFormat = DefaultDocumentFormatRegistry.getFormatByExtension(extension);
         if (documentFormat == null)
             throw new IllegalArgumentException("Неверное расширение файла - " + extension);
-        final LocalOfficeManager officeManager = LocalOfficeManager.install();
+
+        OfficeManager officeManager = LocalOfficeManager.builder()
+                .install()
+                .officeHome("C:\\Program Files (x86)\\OpenOffice 4")
+                .build();
+
+        //final LocalOfficeManager officeManager = LocalOfficeManager.install();
         try {
             // Start an office process and connect to the started instance (on port 2002).
             officeManager.start();
@@ -34,28 +39,14 @@ public class ConverterJod {
         }
     }
 
-    public static void main(String[] args) throws OfficeException {
-        File inputFile = new File("/home/pasha/soft/pdf/document.html");
+    public static void main(String[] args) throws OfficeException, FileNotFoundException {
+        File inputFile = new File("/home/pasha/soft/pdf/document.jpg");
+        String fileName = inputFile.getName();
+        String[] part = fileName.split("\\.");
+        FileInputStream fis = new FileInputStream(inputFile);
         File outputFile = new File("/home/pasha/soft/pdf/document.pdf");
+        FileOutputStream fos = new FileOutputStream(outputFile);
 
-// Create an office manager using the default configuration.
-// The default port is 2002. Note that when an office manager
-// is installed, it will be the one used by default when
-// a converter is created.
-        final LocalOfficeManager officeManager = LocalOfficeManager.install();
-        try {
-
-            // Start an office process and connect to the started instance (on port 2002).
-            officeManager.start();
-
-            // Convert
-            JodConverter
-                    .convert(inputFile)
-                    .to(outputFile)
-                    .execute();
-        } finally {
-            // Stop the office process
-            OfficeUtils.stopQuietly(officeManager);
-        }
+        convertToPdf(fis, part[1], fos);
     }
 }
